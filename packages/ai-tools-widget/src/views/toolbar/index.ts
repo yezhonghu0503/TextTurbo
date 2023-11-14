9700080a6ff35556a944408bccdc7bccf54537a9
yezhonghu0503 <211736726@qq.com>
2023-11-14 17:24:19 +0800

    ⚡

--- packages/ai-tools-widget/src/views/toolbar/index.ts
+++ packages/ai-tools-widget/src/views/toolbar/index.ts
@@ -14,38 +14,51 @@ export class MyElement extends LitElement {
     render() {
         return html`
             <div class="tt_main" draggable="true">
-                <div class="tt_logo"></div>
-                <div class="tt_menu"></div>
+                <div class="tt_tool_main"></div>
+                <div class="tt_bar">
+                    <div class="tt_logo"></div>
+                    <div class="tt_menu"></div>
+                </div>
             </div>
         `;
     }
 
     static styles = css`
         .tt_main {
-            width: 60px;
-            height: 60vh;
-            background-color: #0058aa;
+            width: 700px;
+            height: 70vh;
+            background-color: #0065c4;
             position: fixed;
-            top: 20%;
+            top: 10%;
             right: 5px;
+            z-index: 100;
+            border-radius: 5px;
+            display: flex;
+            justify-content: space-around;
+            padding: 5px 0;
+        }
+        .tt_bar {
+            width: 46px;
+            height: 100%;
             display: flex;
             flex-flow: column;
             justify-content: space-between;
-            align-items: center;
-            z-index: 100;
-            padding: 5px 0;
-            border-radius: 5px;
+            align-items: flex-end;
         }
         .tt_logo {
-            width: 50px;
-            height: 40px;
+            width: 46px;
+            height: 45px;
             background-color: #a9d5ff;
         }
         .tt_menu {
-            width: 50px;
-            height: 55vh;
+            width: 46px;
+            height: 65vh;
             background-color: black;
             border-radius: 5px;
+        }
+        .tt_tool_main {
+            width: 640px;
+            background-color: #a9d5ff;
         }
     `;
 }
