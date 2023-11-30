a84405500509ac96d0d20151d59603011e23fe37
yezhonghu0503 <211736726@qq.com>
2023-11-30 16:21:03 +0800

    fix:add MenuStateController

--- packages/ai-tools-widget/src/controller/MenuStateController.ts
+++ packages/ai-tools-widget/src/controller/MenuStateController.ts
@@ -1,7 +1,9 @@
 import { ReactiveController, ReactiveControllerHost } from "lit";
+import { state } from "lit/decorators.js";
 
 export class MenuStateController implements ReactiveController {
     host: ReactiveControllerHost;
+    @state()
     private menuState: boolean = false;
     constructor(host: ReactiveControllerHost) {
         (this.host = host).addController(this);
